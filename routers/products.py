10494ff5864dda0d0ed5637ea678564154dc31e5
rigo93acosta <rigo93acosta@gmail.com>
2025-01-27 18:24:27 +0000

    Enhance products router with improved endpoint structure and response handling

--- routers/products.py
+++ routers/products.py
@@ -1,9 +1,17 @@
 from fastapi import APIRouter
 
-router_product = APIRouter()
-
-@router_product.get("/products")
-async def products():
-    return ["Product 1", "Product 2", 
+products_list = ["Product 1", "Product 2", 
             "Product 3", "Product 4", 
             "Product 5"]
+
+router_product = APIRouter(prefix="/products",
+                            tags=["products"],
+                           responses={404: {"description": "Not found"}})
+
+@router_product.get("/")
+async def products():
+    return products_list
+
+@router_product.get("/{id}")
+async def product(id: int):
+    return products_list[id]
