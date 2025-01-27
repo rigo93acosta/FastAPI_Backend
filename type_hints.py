fb7cba6d3d85244514e703ca879008ca05a4a7c6
rigo93acosta <rigo93acosta@gmail.com>
2025-01-27 00:19:50 +0000

    Init Project

---
+++ type_hints.py
@@ -0,0 +1,9 @@
+### Type Hints ###
+
+my_string_variable = "My string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_integer_variable = 42
+print(my_integer_variable)
+print(type(my_integer_variable))
